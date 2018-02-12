6668bcc460ac3a3797ad47a8bc5383d472a6c740
Thomas Laird <t6r6l5@gmail.com>
2018-02-12 12:16:05 -0700

    add initial fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import GarageDoor from './components/GarageDoor/GarageDoor';
 import Form from './components/Form/Form';
+import ItemsList from './components/ItemsList/ItemsList';
 import './App.css';
+import { getItems } from './helpers/helpers';
 
 class App extends Component {
   constructor() {
     super();
     
     this.state = {
-      open: false
+      open: false,
+      items: []
     };
+  }
+
+  async componentDidMount () {
+    const { items } = await getItems();
+    this.setState({
+      items
+    })
   }
 
   createGarageDoor() {
     const openClass = this.state.open ? 'open' : 'closed';
-    const buttonText = this.state.open ? 'Lower Door' : 'Raise Door';
     
     return (
       <GarageDoor status={openClass} />
@@ -33,13 +42,21 @@ class App extends Component {
     })
   }
 
+  addItem = (item) => {
+    this.setState({
+      items: [...this.state.items, item]
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <h1>Garage Bin</h1>
+        <h1 className={this.state.open ? 'down' : 'up'}>Garage Bin</h1>
         {this.createGarageDoor()}
         {this.garageDoorButton()}
-        <Form />
+        <Form addItem={this.addItem}/>
+        <ItemsList items={this.state.items}/>
         
       </div>
     );
